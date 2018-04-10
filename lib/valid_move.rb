cf4eb08df8de6b6234387c5c89bf241976686175
Melissa Real <melissa.real@hotmail.com>
2018-04-10 18:25:59 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -13,7 +13,6 @@ end
 def position_taken?(board,index)
  
   if valid_move?(board, index)
- 
    if (board[index]==" " || board[index]=="" || board[index]==nil)
      false
    else (board[index]=="X" || board[index]=="O")
