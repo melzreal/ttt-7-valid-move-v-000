aa524c7c0debfc517ef4f44edac48d70507c681c
Melissa Real <melissa.real@hotmail.com>
2018-04-10 18:38:33 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,9 +1,7 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
   
-  if position_taken?(board, index)
-    false
-  
+  if position_taken?(board, index)==false
     if(index > 0 && index <=8) && (board[index]==""||board[index]==" "|| board[index]==nil)
         true
     else
